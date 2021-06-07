45d927112afc7b128152f7fac046de0ea840abd5
Javiera.H <Javierab.herrera@gmail.com>
2021-06-07 11:28:17 -0400

    Tarjetas en el js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
 console.log(example, data);
+
+for( let i=0; i< data.pokemon.length; i++) {
+    let tarjeta=document.createElement("div");
+    tarjeta.className="Tarjetitas";
+    let nombre=document.createElement("h1");
+    nombre.innerHTML= data.pokemon[i].name;
+    let numero=document.createElement("h2");
+    numero.innerHTML=data.pokemon[i].num;
+    let imagen=document.createElement("img");
+    imagen.src= data.pokemon[i].img;
+   tarjeta.appendChild(nombre);
+   tarjeta.appendChild(numero);
+   tarjeta.appendChild(imagen);
+   document.getElementById("cajacontarjetas").appendChild(tarjeta);
+   
+   }
