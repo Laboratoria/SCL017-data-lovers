a74e7c22540f46f5c6535a9fb8679c5974aeb4a2
Javiera.H <Javierab.herrera@gmail.com>
2021-06-09 15:50:30 -0400

    trabajo en css y js dentro de la trajeta pokemon

--- src/main.js
+++ src/main.js
@@ -1,18 +1,41 @@
+  
 import data from './data/pokemon/pokemon.js';
-console.log(example, data);
+console.log(data);
+for (let i = 0; i < data.pokemon.length; i++) {
 
-for( let i=0; i< data.pokemon.length; i++) {
-    let tarjeta=document.createElement("div");
-    tarjeta.className="Tarjetitas";
-    let nombre=document.createElement("h1");
-    nombre.innerHTML= data.pokemon[i].name;
-    let numero=document.createElement("h2");
-    numero.innerHTML=data.pokemon[i].num;
-    let imagen=document.createElement("img");
-    imagen.src= data.pokemon[i].img;
-   tarjeta.appendChild(nombre);
-   tarjeta.appendChild(numero);
-   tarjeta.appendChild(imagen);
-   document.getElementById("cajacontarjetas").appendChild(tarjeta);
-   
-   }
+    let card = document.createElement("div");
+    card.className = "pokemon-card";
+
+    let pokemonName = document.createElement("div");
+    pokemonName.innerHTML = data.pokemon[i].name;
+    pokemonName.className = "pokemon-name";
+
+    let pokemonNumber = document.createElement("div");
+    pokemonNumber.innerHTML = data.pokemon[i].num;
+    pokemonNumber.className = "pokemon-number";
+
+    let pokemonImagediv = document.createElement("div");
+    let pokemonImage = document.createElement("img");
+    pokemonImage.src = data.pokemon[i].img;
+    pokemonImage.className = "pokemon-image";
+    pokemonImagediv.className = "pokemon-image-div";
+    pokemonImagediv.appendChild(pokemonImage);
+
+
+    data.pokemon[i].type.forEach(function(value){
+        let pokemonType = document.createElement("div");
+        pokemonType.innerHTML =value;
+        pokemonType.className = "pokemon-type-"+value;
+        card.appendChild(pokemonType);
+
+    })
+    card.appendChild(pokemonImagediv);
+    card.appendChild(pokemonNumber);
+    card.appendChild(pokemonName); 
+    
+        document.getElementById("container").appendChild(card);
+  
+ };
+
+
+
