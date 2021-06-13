6279f9664fb5bbeb4f6298529802193339176fe9
Valeria Argomedo <valeria.argomedo@gmail.com>
2021-06-13 19:32:52 -0400

    Arregle una parte de la manipulacion del modal

--- src/main.js
+++ src/main.js
@@ -238,13 +238,15 @@ var btn = document.getElementsByClassName("myBtn");
 var span = document.getElementsByClassName("close");
 
 for(let ii = 0; ii < data.pokemon.length; ii++ ){
-
+    // When the user clicks the button, open the modal 
     btn[ii].onclick = function () {
         modal[ii].style.display = "block";
     }
+    // When the user clicks on <span> (x), close the modal
     span[ii].onclick = function () {
         modal[ii].style.display = "none";
     }
+    // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
         if (event.target == modal[ii]) {
             modal[ii].style.display = "none";
@@ -253,32 +255,3 @@ for(let ii = 0; ii < data.pokemon.length; ii++ ){
     }
 }
 
-/*
-// When the user clicks the button, open the modal 
-btn[0].onclick = function () {
-    modal[0].style.display = "block";
-}
-
-btn[1].onclick = function () {
-    modal[1].style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span[0].onclick = function () {
-    modal[0].style.display = "none";
-}
-
-span[1].onclick = function () {
-    modal[1].style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal[0]) {
-        modal[0].style.display = "none";
-    }
-    if (event.target == modal[1]) {
-        modal[1].style.display = "none";
-    }
-}
-
-
-*/
