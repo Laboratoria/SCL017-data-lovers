a8ab69984f45d9370abab979c2daac1ef66b5431
Nicole Quijada <quijadabnicolea@gmail.com>
2021-06-14 19:45:37 -0400

    pruebas

--- src/data.js
+++ src/data.js
@@ -1,9 +1,14 @@
 // estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+const namePokemon =[];
+export const pokemon = (namePokemon) => {
+  return pokemon.filter((pokemon) => 
+  pokemon.name == namePokemon);
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
+
+// var namePokemon = pokemon.filter(function (pokemon) {
+//   return pokemon.name;
+// });
