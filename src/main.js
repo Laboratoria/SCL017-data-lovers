4dc598ccdbfa98b5b8f1d0aea5c32dfd87297b1b
Paula <paularamirezsot@gmail.com>
2021-06-14 23:41:51 -0400

    subiendo cambios

--- src/main.js
+++ src/main.js
@@ -125,12 +125,45 @@ import data from './data/pokemon/pokemon.js';
 
 
 
-//console.log(example, data);
 
-//const pokemon = data.pokemon
-//console.log(pokemon);
 
-//for(let i=0; i<pokemon.length; i++){
-//console.log(pokemon["name"]);
-//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
