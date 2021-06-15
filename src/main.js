121c710014dfa05965d79ab34cd8db9b5a4dd1fd
Paula <paularamirezsot@gmail.com>
2021-06-14 23:13:11 -0400

    comento codigo

--- src/main.js
+++ src/main.js
@@ -3,4 +3,12 @@ import { example } from './data.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
+
+//const pokemon = data.pokemon
+//console.log(pokemon);
+
+//for(let i=0; i<pokemon.length; i++){
+//console.log(pokemon["name"]);
+//}
+
