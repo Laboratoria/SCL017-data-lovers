e128bb3e8b7a3eb4cbd9d93c4f9c454770faaa72
Nicole Quijada <quijadabnicolea@gmail.com>
2021-06-16 17:05:03 -0400

    ocultado logo segunda pantalla

--- src/main.js
+++ src/main.js
@@ -10,11 +10,8 @@ const start =  document.getElementById('start');
 const togo = () => {
     document.getElementById('home').className = " hide container " 
     document.getElementById('second-window').className = "second-window "
-<<<<<<< HEAD
     document.getElementById('logo').className = "hide"
-=======
-    document.getElementById("logo").className = "hide"
->>>>>>> master
+
 }
 
 start.addEventListener("click", togo, false);
