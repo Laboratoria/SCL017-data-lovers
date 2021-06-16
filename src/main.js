f21d62d519b038ce8131be27406db1c99e9ebc4e
Nicole Quijada <quijadabnicolea@gmail.com>
2021-06-16 19:10:17 -0400

    cambio logo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import dataPokemon from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
-console.log(data);
+console.log(dataPokemon);
 
 
 const start =  document.getElementById('start');
@@ -11,8 +11,32 @@ const togo = () => {
     document.getElementById('home').className = " hide container " 
     document.getElementById('second-window').className = "second-window "
     document.getElementById('logo').className = "hide"
-
 }
 
 start.addEventListener("click", togo, false);
 
+let pokemonList = document.getElementById('pokemonList');
+
+
+let poke = function(dataPokemon){
+    let newDiv = document.createElement('div');
+    let newBtnId = document.createElement('button');
+    let newImg = document.createElement('img');
+    let newBtntype = document.createElement('button');
+    let newBtnPower = document.createElement('button');
+    newBtnId.innerText = dataPokemon.num;
+    newImg.innerHTML = dataPokemon.img;
+    newBtnType.innerText = dataPokemon.type;
+    newBtnPower.innerText = dataPokemon.type;
+    newDiv.appendChild(newBtnId);
+    newDiv.appendChild(newImg);
+    newDiv.appendChild(newBtnType);
+    newDiv.appendChild(newBtnPower);
+    pokemonList.appendChild(newDiv);
+
+
+}
+
+for(let i = 0; i < dataPokemon.length; i++){
+    poke(pokemonList[i]);
+}
