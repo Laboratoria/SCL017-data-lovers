0ea5db5d7148ccafd977ea6e16feb394c2d7e7a5
SofiaCanalesMoya <sofia.sbcm@gmail.com>
2021-06-18 20:34:05 -0400

    cambio prueba data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
+const data = data.results;
+    for(let i=0;i<data.length; i++){
+        if( )
+    }
+const = {
+    results: <
+}
 console.log(example, data);
