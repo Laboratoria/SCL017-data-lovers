beaa72de5f7402deaa9293bfd55f27410a8e7c40
SofiaCanalesMoya <sofia.sbcm@gmail.com>
2021-06-21 09:51:07 -0400

    Muestra de personajes en la pantalla

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 import { example } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
-const data = data.results;
-    for(let i=0;i<data.length; i++){
-        if( )
-    }
+const { info, results } = data;
+for (let i = 0; i <results.length; i++) {
+    const personajes = results[i]
+
+    const caja = document.createElement("div")
+    caja.className = "personaje";
+
+    const cajaNombre = document.createElement("div")
+    cajaNombre.className = "nombre";
+    const texto = document.createTextNode(personajes.name);
+    cajaNombre.appendChild(texto);
+    caja.appendChild(cajaNombre)
+
+    const itemImg = document.createElement("img")
+    itemImg.src = personajes.image
+    itemImg.className = "imagen"
+    caja.appendChild(itemImg);
+
+    document.getElementById("root").appendChild(caja)
+}
 
 console.log(example, data);
