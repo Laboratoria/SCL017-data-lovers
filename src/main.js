9dae7962e7b2e87048aa25fb573dd298a1ee3085
Paula <paularamirezsot@gmail.com>
2021-06-23 10:14:15 -0400

    antes del colapso

--- src/main.js
+++ src/main.js
@@ -8,8 +8,6 @@ console.log(example, data);
 console.log("correcto");
 const pokemon= data.pokemon;
 
-
-
     document.getElementById("boton").addEventListener("click",bringData);
     const containerRoot= document.getElementById("root");
     function bringData(){
@@ -17,50 +15,24 @@ const pokemon= data.pokemon;
 
     for (let i=0; i<pokemon.length; i++){
         console.log(pokemon[i].name)
-        containerRoot.innerHTML += `       <div class="containerPokemon" id= "pokemon"+ ${pokemon[i].num}> 
-                                            <img src=${pokemon[i].img}>
-                                            <p class="pokedex">#${pokemon[i].num}</p>
-                                            <p>${pokemon[i].name}</p>
-                                            <p>${pokemon[i].type}</p>
-                                            <p>${pokemon[i].generation.name}</p>
-                                            
-                                        
+        containerRoot.innerHTML += `
 
-                                        </div>
-                                    `;
-
-                                    const query = document.querySelector("#pokemon" + `${pokemon[i].num}`);
-                                                    
- };                                          
-            
-    const pokemonContainer = query;
-    const flipCard = document.createElement("div");
-    flipCard.classList.add("flip-card");
-
-     const cardContainer = document.createElement("div");
-    cardContainer.classList.add("card-container");
-
-    flipCard.appendChild(cardContainer);
-
-    console.log(pokemon)
-
-    const card = document.createElement("div");
-    card.classList.add ("pokemon-block");
-
-    const cardBack = document.createElement("div");
-    cardBack.classList.add("pokemon-block-back");
-    cardBack.textContent = "Super poder";
-
-    
-
-    cardContainer.appendChild(card);
-        cardContainer.appendChild(cardBack);
-        pokemonContainer.appendChild(flipCard); 
-
-
-
-
-
+        <div class="flip-card"  id="pokemon${pokemon[i].num}">
+            <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <img src=${pokemon[i].img}>
+                <p class="pokedex"># ${pokemon[i].num}</p>
+                <p>${pokemon[i].name}</p>
+            </div>
+            <div class="flip-card-back">
+                <p>${pokemon[i].type}</p>
+                <p>${pokemon[i].generation.name}</p>
+            </div>
+        </div>
+        </div>
+    `;
+ };
+}
 
 
 
@@ -82,14 +54,6 @@ const pokemon= data.pokemon;
     cardContainer.appendChild(card);
     cardContainer.appendChild(cardBack);
     containerRoot.appendChild(flipCard); */
-    
-
-
-
-
-
-
-    //document.querySelector("#boton").addEventListener("click", bringData);
 
 //function bringData(){
  //   console.log("dentro de la funcion")
@@ -118,7 +82,7 @@ const pokemon= data.pokemon;
 //show.appendChild(li)
 //console.log(li)
 
-    }
+    
 
 
 
