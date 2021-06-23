cca7666b7dc65b94c902236331d25e4965147cd0
Marianny Garcia <mdvgarciar@gmail.com>
2021-06-23 10:26:41 -0400

    main

--- src/main.js
+++ src/main.js
@@ -1,9 +1,1 @@
-import data from './data/pokemon/pokemon.js';
-import  {filterByGeneration}  from './data.js';
-
-//Variable para guardar la data
-const listaPokemon = data.pokemon;
-
-console.log(filterByGeneration(listaPokemon, ))
-
 
