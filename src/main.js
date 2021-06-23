d3644ad36e1420c4f64597118bff6d599687246b
NicolePozo <nicolepozo.salas@gmail.com>
2021-06-23 11:34:53 -0400

    no sangran ojos

--- src/main.js
+++ src/main.js
@@ -14,39 +14,32 @@ const containerRoot= document.getElementById("root");
 const searchPo = document.getElementById("searchPo");
 searchPo.addEventListener("keyup",(e)=> {
     //containerRoot.innerHTML= ""
-   const searchString= e.target.value.toLowerCase();
-console.log(searchString);
-const filteredPokemon = pokemon.filter(element=>{
-  //  console.log(element.name)
-    return element.name.toLowerCase().includes(searchString);
-});
-//console.log(filteredPokemon);//que imprima en html//
-for (let i=0; i<filteredPokemon.length; i++){
-containerRoot.innerHTML =`<div class="flip-card" id= "pokemon${pokemon[i].num}">
-<div class ="flip-card-inner">
-<div class ="flip-card-front"> 
-    <img src=${pokemon[i].img}>
-    <p class="pokedex">#${pokemon[i].num}</p>
-    <p>${pokemon[i].name}</p>
-</div>
-<div class="flip-card-back">
-<p>${pokemon[i].type}</p>
-<p>${pokemon[i].generation.name}</p>
-</div>
-</div>
-</div>`
-
-}
-//containerRoot.innerHTML= filteredPokemon;
-
-   /*const filteredPokemon= pokemon.filter((element) =>{
-     return (
-         element.name.toLowerCase().includes(searchString)
-     );
-
+    const searchString= e.target.value.toLowerCase();
+    console.log(searchString);
+    const filteredPokemon = pokemon.filter(element=>{
+        return element.name.toLowerCase().includes(searchString);
     });
- console.log(filteredPokemon); 
- */
+    for (let i=0; i<filteredPokemon.length; i++){
+        containerRoot.innerHTML =`<div class="flip-card">
+                                    <div class ="flip-card-inner">
+                                        <div class ="flip-card-front"> 
+                                            <img src=${filteredPokemon[i].img}>
+                                            <p class="pokedex">#${filteredPokemon[i].num}</p>
+                                            <p>${filteredPokemon[i].name}</p>
+                                        </div>
+                                        <div class="flip-card-back">
+                                            <p>${filteredPokemon[i].type}</p>
+                                            <p>${filteredPokemon[i].generation.name}</p>
+                                        </div>
+                                    </div>
+                                </div>`
+                               
+     if(searchString == "") {
+        return(bringData);
+       }
+       
+    
+    }
 });
 
 
