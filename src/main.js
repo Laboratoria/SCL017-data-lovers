5cfbdf554b84b307450145d64aeaab3c7ab7560f
Marianny Garcia <mdvgarciar@gmail.com>
2021-06-23 20:29:34 -0400

    test console

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
+import pokemon from "./data/pokemon/pokemon.js";
 
+const inputValue = document.getElementById("searchpokemon");
+const matchingPokesDiv = document.getElementById("matching");
+// const buttonClick = document.getElementById("btn");
+
+inputValue.addEventListener("keyup", (event) => {
+
+    if (inputValue.value.length >= 1) {
+    const inputValueLowerCase = inputValue.value.toLowerCase();
+    console.log ( pokemon.pokemon
+        .filter((pkm) => pkm.name.startsWith(inputValueLowerCase)))
+    }
+});
