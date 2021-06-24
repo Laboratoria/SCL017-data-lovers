d4115a560edaaecf46c4b58bdc0f16c468bc3f31
Solange Gonzalez <solgonzalezaraya@gmail.com>
2021-06-24 10:37:41 -0400

    pruebas funcion filtrar

--- src/main.js
+++ src/main.js
@@ -1,18 +1,54 @@
 import data from './data/pokemon/pokemon.js';
 import  {}  from './data.js';
-import pokemon from './data/pokemon/pokemon.js';
+
 
 
 const dataPokemon = data.pokemon;
-console.log(dataPokemon);
 
-dataPokemon.forEach(function(pokemon){
-    console.log(pokemon.name);
-});
+const barraBusqueda = document.getElementById("searchpokemon");
+const btnBuscarPokemon = document.getElementById("btn-buscar");
+const btnVerlosTodos = document.getElementById("btn-verlosTodos");
+const resultado = document.getElementById("resultado");
 
-//Funcion para que boton buscar cambie de pagina
-document.getElementById("btn-buscar").addEventListener("click", PokemonPage);
-function PokemonPage(){
-    document.getElementById("infoPokemonPage").style.display = "block";
+// Funcion para cambiar a pagina donde se despliegan todos
+const verlosTodos = () => {
+    document.getElementById("paginaVerTodos").style.display = "block";
     document.getElementById("firstPage").style.display = "none";
-    };
+};
+btnVerlosTodos.addEventListener("click", verlosTodos);
+
+// Funcion para cambiar de pantalla donde aparece info de un pokemon
+const cambiarPagina = () => {
+    document.getElementById("paginaPokemon").style.display = "block";
+    document.getElementById("firstPage").style.display = "none";
+};
+btnBuscarPokemon.addEventListener("click", cambiarPagina, false);
+
+//Funcion para barra de busqueda
+const filtrar = () => {
+ 
+    resultado.innerHTML = " ";
+
+    const pokeElegido = barraBusqueda.value.toLowerCase();
+    for( let pokemon of dataPokemon){
+        let nombre = pokemon.name.toLowerCase();
+        if(nombre.indexOf(pokeElegido) !== -1){
+            resultado.innerHTML += `
+            <h2 class="nombrePoke">${pokemon.name}</h2>
+            <p>${pokemon.type}</p>
+            <img src="${pokemon.img}" ></img>
+            <p>${pokemon.about}</p>
+
+            `
+        }
+    }
+    if(resultado.innerHTML === " "){
+        resultado.innerHTML +=`
+        <h2>Pokemon no encontrado</h2>
+        `
+    }
+    
+};
+btnBuscarPokemon.addEventListener("click", filtrar, false);
+
+
