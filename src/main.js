c820e3e878cd93af5e0cfca80fc271e52ba75d87
Solange Gonzalez <solgonzalezaraya@gmail.com>
2021-06-28 19:36:30 -0400

    primer parte de tarejta pokemon sin css final

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import pokemon from "./data/pokemon/pokemon.js";
 
+const dataPokemon = pokemon.pokemon;
 const inputValue = document.getElementById("searchpokemon");
 const matchingPokesDiv = document.getElementById("matching");
 const buttonClick = document.getElementById("btn");
@@ -13,9 +14,27 @@ inputValue.addEventListener("keyup", (event) => {
       .forEach((poke) => {
         let matchingPoke = document.createElement("div");
         matchingPoke.className = "pokeNameStyle";
+        //Click a pokemon en barra de busqueda para ir a info Pokemon
         matchingPoke.onclick = function () {
           pokeStats(poke);
+          pokeStatsContainer.innerHTML = `
+        
+            <div class="pokemonCard">
+              <p class="poke-card-num">#${poke.num}</p>
+              <h2>${poke.name}</h2>
+              <img src="${poke.img}">
+              <div class="card-square-info">
+                <p class="pokemon-card-about"> ${poke.about}</p>
+                <p class="poke-card-type"> type: ${poke.type}.<p>
+                <p class="poke-card-resistance"> Fuerte frente a: ${poke.resistant}</p>
+                <p class="poke-card-resistance"> Debil frente a: ${poke.weaknesses}</p>
+              </div>
+            </div>
+            <button type="button" class="btn" id="btn-volver">Vuelve a atrás</button>
+          
+          `;
         };
+        //Lista en barra de búsqueda
         matchingPoke.innerHTML = `<img class="pokeImgDisplay" src="${poke.img}">
                                   <p class= "pokeTxtDisplay">${poke.name}</p>`;
         matchingPokesDiv.appendChild(matchingPoke);
@@ -44,3 +63,5 @@ function pokeStats(nameClicked) {
     document.getElementById("showAllPokemon").style.display = "none";
     document.getElementById("pokeStatsContainer").style.display = "block";
 }
+
+
